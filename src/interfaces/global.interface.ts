dd128d77deef40bf414524a5671687358f68f392
ghislaingenay <ghislain.gny@gmail.com>
2023-03-26 02:53:12 +0700

    Add status code interface to module

--- src/interfaces/global.interface.ts
+++ src/interfaces/global.interface.ts
@@ -5,6 +5,16 @@ export interface IGetServerSideData<T = any, K = any> {
   statusCode?: number;
 }
 
+export enum EStatusCodes {
+  SUCCESS = 200,
+  CREATED = 201,
+  VALIDATION_NODE = 400,
+  DATABASE_CONNECTION = 500,
+  BAD_REQUEST = 400,
+  NOT_AUTHORIZED = 401,
+  NOT_FOUND = 404,
+}
+
 export enum ECurrency {
   USD = 'USD',
   EUR = 'EUR',
