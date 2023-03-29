f8d23ce973b885bcc9222106c4bbcc3a8081541e
ghislaingenay <ghislain.gny@gmail.com>
2023-03-29 09:58:42 +0700

    MOD Add pool to module + add close, query fn + fix pool type

--- src/definitions/pool.pg.ts
+++ src/definitions/pool.pg.ts
@@ -1,13 +1,25 @@
 import { Pool, PoolConfig } from 'pg';
 
 class PGPool {
-  _pool = null;
+  _pool: any = null;
 
   connect(options: PoolConfig) {
     console.log('Connecting to Postgres...');
-    this._pool = new Pool(options);
+    this._pool = new Pool({
+      ...options,
+      connectionTimeoutMillis: 0,
+      idleTimeoutMillis: 0,
+    });
     this._pool.connect();
     return this._pool.query('SELECT 1 + 1');
+  }
+
+  close() {
+    return this._pool.end();
+  }
+
+  query(query: string, params?: any[]) {
+    return this._pool.query(query, params);
   }
 }
 
