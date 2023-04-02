18ba9187693419f77a634eccca8e0fcf07f111fc
ghislaingenay <ghislain.gny@gmail.com>
2023-04-02 15:17:22 +0700

    Add uplod as groupId and microservice name

--- src/events/subjects.interface.ts
+++ src/events/subjects.interface.ts
@@ -1,15 +1,17 @@
 export enum ETopics {
-  PICTURE_CREATED = 'picture:created',
-  PICTURE_UPDATED = 'picture:updated',
-  PICTURE_DELETED = 'picture:deleted',
+  PICTURE_CREATED = "picture:created",
+  PICTURE_UPDATED = "picture:updated",
+  PICTURE_DELETED = "picture:deleted",
 }
 
 export enum EMicroServices {
-  PICTURE = 'picture',
+  PICTURE = "picture",
+  UPLOAD = "upload",
 }
 
 export enum EGroupId {
-  PICTURE = 'picture-group',
+  PICTURE = "picture-group",
+  UPLOAD = "upload-group",
 }
 
 export interface IEvent<T> {
