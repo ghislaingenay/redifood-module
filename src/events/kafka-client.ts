cf74d82d0657259f8123bca6084c777a1404d8b3
ghislaingenay <ghislain.gny@gmail.com>
2023-04-03 23:04:09 +0700

    Check log when connecting to kafka client

--- src/events/kafka-client.ts
+++ src/events/kafka-client.ts
@@ -1,5 +1,5 @@
-import { Logger } from '@nestjs/common';
-import { Kafka } from 'kafkajs';
+import { Logger } from "@nestjs/common";
+import { Kafka } from "kafkajs";
 
 class KafkaClient {
   private kafka: Kafka;
@@ -9,15 +9,16 @@ class KafkaClient {
     return new Promise((resolve, reject) => {
       try {
         this.kafka = new Kafka({
-          clientId: 'redifood',
+          clientId: "redifood",
           // Therefore the clientId should be shared across multiple instances in a cluster or horizontally scaled application, but distinct for each application.
           brokers: [broker],
           connectionTimeout: 3000, // Time in milliseconds to wait for a successful connection
           requestTimeout: 25000, // Time in milliseconds to wait for a successful request.
         });
+        this.logger.log("Connected to kafka");
         resolve(this.kafka);
       } catch (err) {
-        this.logger.error('failed to connect to kafka', err);
+        this.logger.error("failed to connect to kafka", err);
         reject(err);
       }
     });
