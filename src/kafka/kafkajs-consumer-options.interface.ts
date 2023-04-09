199ffa1499339af331735dc85f89ab92d62e524f
ghislaingenay <ghislain.gny@gmail.com>
2023-04-09 18:00:40 +0700

    Remove kafka folder from modules

--- src/kafka/kafkajs-consumer-options.interface.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-//@ts-ignore
-import { ConsumerConfig, ConsumerSubscribeTopics, KafkaMessage } from 'kafkajs';
-
-export interface IKafkaJsConsumerOptions {
-  topic: ConsumerSubscribeTopics;
-  config: ConsumerConfig;
-  onMessage: (message: KafkaMessage) => Promise<void>;
-}
