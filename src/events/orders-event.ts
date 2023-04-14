0ab5d1d38aaacd1307917256e5c0db81104f9849
ghislaingenay <ghislain.gny@gmail.com>
2023-04-14 20:54:28 +0700

    Fix order event + add payment srv

--- src/events/orders-event.ts
+++ src/events/orders-event.ts
@@ -1,8 +1,14 @@
 import { IOrderApi } from '../interfaces';
 
+// Update foods srv
 export class OrderStateEvent {
   constructor(
     public readonly orderId: IOrderApi['id'],
     public readonly orderStatus: IOrderApi['orderStatus'],
   ) {}
+}
+
+// send to payment srv
+export class OrderAwaitPaymentEvent {
+  constructor(public readonly order: IOrderApi) {}
 }
