5ddb7b23211804bf39d14eeeeb2208dcf00e88d9
ghislaingenay <ghislain.gny@gmail.com>
2023-04-15 13:34:49 +0700

    Remove toString method (not used in TCP transport)

--- src/events/picture/picture-class.event.ts
+++ src/events/picture/picture-class.event.ts
@@ -5,37 +5,13 @@ export class PhotoCreatedEvent {
     public readonly item_id: IFoodDB['id'],
     public readonly photo_url: IFoodDB['item_photo'],
   ) {}
-
-  toString(): string {
-    return JSON.stringify({
-      item_id: this.item_id,
-      photo_url: this.photo_url,
-    });
-  }
 }
 export class PhotoUpdatedEvent {
   constructor(
     public readonly item_id: IFoodDB['id'],
     public readonly photo_url: IFoodDB['item_photo'],
   ) {}
-
-  toString(): string {
-    return JSON.stringify({
-      item_id: this.item_id,
-      photo_url: this.photo_url,
-    });
-  }
 }
 export class PhotoDeletedEvent {
-  constructor(
-    public readonly item_id: IFoodDB['id'],
-    public readonly photo_url: IFoodDB['item_photo'],
-  ) {}
-
-  toString(): string {
-    return JSON.stringify({
-      item_id: this.item_id,
-      photo_url: this.photo_url,
-    });
-  }
+  constructor(public readonly item_id: IFoodDB['id']) {}
 }
