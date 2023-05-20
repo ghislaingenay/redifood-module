e1b2992b023fd9cf8a780aa677acc82d54ed84cd
ghislaingenay <ghislain.gny@gmail.com>
2023-05-20 15:19:42 +0700

    Add a payment status ENUM

--- src/interfaces/payment.interface.ts
+++ src/interfaces/payment.interface.ts
@@ -1,4 +1,11 @@
 export enum EPaymentType {
-  CASH = "cash",
-  CARD = "card",
+  CASH = 'cash',
+  CARD = 'card',
+}
+
+export enum EPaymentStatus {
+  CANCELLED = 'cancelled',
+  REJECTED = 'rejected',
+  AWAITING = 'awaiting payment',
+  COMPLETED = 'completed',
 }
