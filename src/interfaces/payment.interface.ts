622ca3a75c6e095379cce46d4fcf4d002354f670
ghislaingenay <ghislain.gny@gmail.com>
2023-05-21 14:19:37 +0700

    Add payment interface for PaymentsRepo

--- src/interfaces/payment.interface.ts
+++ src/interfaces/payment.interface.ts
@@ -1,3 +1,5 @@
+import { UserPayload } from './auth.interface';
+
 export enum EPaymentType {
   CASH = 'cash',
   CARD = 'card',
@@ -8,4 +10,31 @@ export enum EPaymentStatus {
   REJECTED = 'rejected',
   AWAITING = 'awaiting payment',
   COMPLETED = 'completed',
+}
+
+export interface IPaymentDB {
+  id?: number;
+  user_id: UserPayload['id'];
+  order_id: number;
+  payment_stripe_id?: string;
+  payment_status: EPaymentStatus;
+  payment_type: EPaymentType;
+  payment_amount: number;
+  payment_date: Date;
+  payment_discount_applied: boolean;
+  payment_discount_id: number;
+  payment_tax_amount: number;
+}
+export interface IPaymentApi {
+  id?: number;
+  userId: UserPayload['id'];
+  orderId: number;
+  paymentStripeId?: string;
+  paymentStatus: EPaymentStatus;
+  paymentType: EPaymentType;
+  paymentAmount: number;
+  paymentDate: Date;
+  paymentDiscountApplied: boolean;
+  paymentDiscountId: number;
+  paymentTaxAmount: number;
 }
