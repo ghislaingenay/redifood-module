bb81f5d653934d92165fe4fbd100d5f4a16daed8
ghislaingenay <ghislain.gny@gmail.com>
2023-06-28 17:33:48 +0700

    Add IGetHistoryOrders interface

--- src/interfaces/history.interface.ts
+++ src/interfaces/history.interface.ts
@@ -1,6 +1,14 @@
+import { IPagination } from './global.interface';
+import { IOrderApi } from './orders.interface';
+
 export type TGetHistoryParams = {
   startDate: Date;
   endDate: Date;
   page: number;
   results: number;
 };
+
+export interface IGetHistoryOrders {
+  meta: IPagination;
+  orders: IOrderApi<string>[];
+}
