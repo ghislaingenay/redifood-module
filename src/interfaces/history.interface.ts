3eb43f7433f8eade93826223d00dda4c17f4f147
ghislaingenay <ghislain.gny@gmail.com>
2023-06-30 11:01:19 +0700

    Add startDate and endDate to optional in history interface

--- src/interfaces/history.interface.ts
+++ src/interfaces/history.interface.ts
@@ -1,9 +1,9 @@
-import { IPagination } from './global.interface';
-import { IOrderApi } from './orders.interface';
+import { IPagination } from "./global.interface";
+import { IOrderApi } from "./orders.interface";
 
 export type TGetHistoryParams = {
-  startDate: Date;
-  endDate: Date;
+  startDate?: Date;
+  endDate?: Date;
   page: number;
   results: number;
 };
