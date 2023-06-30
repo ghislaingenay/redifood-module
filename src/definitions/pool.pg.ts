83dca57a5bbc704f9c1d3516bcf5cb0e97b7e70e
ghislaingenay <ghislain.gny@gmail.com>
2023-06-30 15:06:41 +0700

    Fix formatting with Prettier

--- src/definitions/pool.pg.ts
+++ src/definitions/pool.pg.ts
@@ -1,17 +1,17 @@
-import { Pool, PoolConfig } from 'pg';
+import { Pool, PoolConfig } from "pg";
 
 class PGPool {
   _pool: any = null;
 
   connect(options: PoolConfig) {
-    console.log('Connecting to Postgres...');
+    console.log("Connecting to Postgres...");
     this._pool = new Pool({
       ...options,
       connectionTimeoutMillis: 0,
       idleTimeoutMillis: 0,
     });
     this._pool.connect();
-    return this._pool.query('SELECT 1 + 1');
+    return this._pool.query("SELECT 1 + 1");
   }
 
   close() {
