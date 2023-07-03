a9785176e972652304ee791032ff836c279d3a1e
ghislaingenay <ghislain.gny@gmail.com>
2023-07-03 09:36:41 +0700

    Move information to backend folder

--- src/handling-nestjs/database-error.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class DatabaseError extends HttpException {
-  constructor() {
-    super('Database connection error', HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-}
