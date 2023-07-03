bcd454a06e1bb9d017e8b5a5fa838151a248fea8
ghislaingenay <ghislain.gny@gmail.com>
2023-07-03 09:14:27 +0700

    Fix system to be able to build app

--- src/events/kafka-client.ts
+++ src/events/kafka-client.ts
@@ -1,30 +1,31 @@
-import { Logger } from '@nestjs/common';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-import { Kafka } from 'kafkajs';
+// import { Logger } from '@nestjs/common';
+// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// //@ts-ignore
+// import { Kafka } from 'kafkajs';
 
-class KafkaClient {
-  private kafka: Kafka;
-  private readonly logger!: Logger;
+// class KafkaClient {
+//   private kafka: Kafka;
+//   private readonly logger!: Logger;
 
-  connect(broker: string) {
-    return new Promise((resolve, reject) => {
-      try {
-        this.kafka = new Kafka({
-          clientId: 'redifood',
-          // Therefore the clientId should be shared across multiple instances in a cluster or horizontally scaled application, but distinct for each application.
-          brokers: [broker],
-          connectionTimeout: 3000, // Time in milliseconds to wait for a successful connection
-          requestTimeout: 25000, // Time in milliseconds to wait for a successful request.
-        });
-        this.logger.log('Connected to kafka');
-        resolve(this.kafka);
-      } catch (err) {
-        this.logger.error('failed to connect to kafka', err);
-        reject(err);
-      }
-    });
-  }
-}
+//   connect(broker: string) {
+//     return new Promise((resolve, reject) => {
+//       try {
+//         this.kafka = new Kafka({
+//           clientId: 'redifood',
+//           // Therefore the clientId should be shared across multiple instances in a cluster or horizontally scaled application, but distinct for each application.
+//           brokers: [broker],
+//           connectionTimeout: 3000, // Time in milliseconds to wait for a successful connection
+//           requestTimeout: 25000, // Time in milliseconds to wait for a successful request.
+//         });
+//         this.logger.log('Connected to kafka');
+//         resolve(this.kafka);
+//       } catch (err) {
+//         this.logger.error('failed to connect to kafka', err);
+//         reject(err);
+//       }
+//     });
+//   }
+// }
 
-export const kafkaClient = new KafkaClient();
+// export const kafkaClient = new KafkaClient();
+export {};
